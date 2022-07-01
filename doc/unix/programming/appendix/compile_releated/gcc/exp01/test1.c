0f61d4150c5e8df6fa0598e1e610d5f0e523da60
Leo Victor <zhangguoliang@tbank.com>
2022-07-01 11:43:07 +0800

    update

--- doc/unix/programming/appendix/compile_releated/gcc/exp01/test1.c
+++
@@ -1,3 +0,0 @@
-int test1(int a) {
-  return a + 1;
-}
--- doc/unix/shell/section/demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-ls *.org | sed "s/\.org//" |
-while IFS= read -r line; do
-    echo $line
-    echo "---"
-    mv ${line}".org" ${line}
-done
